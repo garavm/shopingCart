bb2c981a778f4c73f87d6feb7f0de81709b78921
garavm <mohapatragaurav9@gmail.com>
2018-06-04 22:16:35 +0530

    added csvtojson function

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    data: []
+  };
+
+  convertCsvtoObject = csv => {
+    const lines = csv.split("\n");
+    const [headerText] = lines.splice(0, 1);
+    const headers = headerText.split(",");
+    const result = lines.map(line => {
+      const items = line.split(",");
+      return items.reduce((accum, item, index) => {
+        const key = headers[index];
+        accum[key] = item;
+        return accum;
+      }, {});
+    });
+    return result;
+  };
+
+  componentDidMount() {
+    fetch(
+      "https://raw.githubusercontent.com/radik909/cart/master/src/MOCK_DATA.csv"
+    )
+      .then(res => res.text())
+      .then(csv => {
+        this.setState({
+          data: this.convertCsvtoObject(csv)
+        });
+      });
+  }
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const MyComponent = () => {
+
+    {
+        return(
+            <h2>Way to go you just created a component!!</h2>
+        );
+
+
+    }
+
+}
+
+export default MyComponent;
